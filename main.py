a0986e55d1c0b72059f897fa20a9db81cbbd153a
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-24 14:12:08 -0500

    Created classes for project and welcome method

---
+++ main.py
@@ -0,0 +1,6 @@
+from rpsls_game import Rpsls_game
+if __name__ == '__main__':
+    game = Rpsls_game()
+
+
+game.display_welcome()
---
+++ rpsls_game.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+class Rpsls_game:
+
+    def __init__(self):
+        pass
+
+
+    def display_welcome(self):
+        print("""
+        ***************************************************
+        Welcome to Rock Paper Scissors Lizard Spock!!!
+        ***************************************************
+        """)
+        print("Each game will be the best of three games")
+
+        print("\n\nHere are the gesture conditions:\n\nScissors cut Paper\nPaper covers Rock\nLizard poisons Spock\nSpock smashes scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\nRock crushes Scissors")
+
+
