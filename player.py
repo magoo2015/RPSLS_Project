7b045d2752852cb642534b21145c2da45c8883f7
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-24 14:23:04 -0500

    created rules method

---
+++ player.py
@@ -0,0 +1,8 @@
+
+
+class Player:
+
+    def __init__(self,player, gestures, wins):
+        self.player = player
+        self.gestures = []
+        self.wins = 0
--- rpsls_game.py
+++ rpsls_game.py
@@ -12,12 +12,16 @@ class Rpsls_game:
 
     def display_welcome(self):
         print("""
-        ***************************************************
+        **********************************************
         Welcome to Rock Paper Scissors Lizard Spock!!!
-        ***************************************************
+        **********************************************
         """)
-        print("Each game will be the best of three games")
+       
 
-        print("\n\nHere are the gesture conditions:\n\nScissors cut Paper\nPaper covers Rock\nLizard poisons Spock\nSpock smashes scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\nRock crushes Scissors")
+    def display_rules(self):
+         print("Each game will be the best of three games")
+
+         print("\n\nHere are the gesture conditions:\n\nScissors cut Paper\nPaper covers Rock\nLizard poisons Spock\nSpock smashes scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\nRock crushes Scissors")
+
 
 
