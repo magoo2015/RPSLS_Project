76f0444684e26ba16680620a9cab735187e5e90a
magoo2015 <mcgee.sydney@gmail.com>
2022-05-25 13:07:44 -0500

    add human vs human, human vs ai, and ai vs ai methods

--- players.py
+++ players.py
@@ -10,5 +10,12 @@ class Player:
 
     
     def get_gestures(self):
-        self.gestures = input("Choose your gesture.")
+        print("""
+        Choose 0 for Rock.
+        Choose 1 for Paper.
+        Choose 2 for Scissors.
+        Choose 3 for Lizard.
+        Choose 4 for Spock.
+        """)
+        self.gestures = int(input("Choose your gesture. "))
         return self.gestures
--- rpsls_game1.py
+++ rpsls_game1.py
@@ -10,10 +10,13 @@ class Rpsls_game:
 
     def __init__(self):
         self.player_1 = Player("Player 1", 0)
-        #self.player_2 = Player("Player 2", 0)
+        self.player_2 = Player("Player 2", 0)
         self.AI_1 = Player("AI_one", 0)
-        #self.AI_2 = Player("AI_two", 0)
+        self.AI_2 = Player("AI_two", 0)
         self.num_of_players = 0
+        self.player = ""
+        self.ai = ""
+
 
 
     def display_welcome(self):
@@ -39,48 +42,42 @@ class Rpsls_game:
          Spock vaporizes Rock
          Rock crushes Scissors
          """)
-    
 
     def choose_players(self):
-       self.num_of_players = input("How many players? ")
-       return self.num_of_players
 
-        
+        self.num_of_players = input("How many players? Choose 1, 2, or 3 for AI vs AI ! ")
+        print(type(self.num_of_players))
 
-    def human_ai(self):
-        pass
+    
+        if self.num_of_players == "1":
+            return self.human_vs_ai()
+        elif self.num_of_players == "2":
+            return self.human_vs_human()
+        elif self.num_of_players == "3":
+            return self.ai_vs_ai()
 
-    #maybe just a human and ai method?
-    def human_human(self, player):
-        self.player_1 = player
-        pass
+    def human_vs_ai(self):
+        self.player_1 = Human("Player1", 0)
+        self.AI_1 = Ai("Ai", 0)
 
+    
+    def human_vs_human(self):
+        self.player_1 = Human("Player1", 0)
+        self.player_2 = Human("Player2",0)
 
-    def ai_ai(self, player):
-        self.AI_1 = player
-        pass
+    def ai_vs_ai(self):
+        self.AI_1 = Ai("Ai", 0)
+        self.AI_2 = Ai("Ai_2", 0)
+
+    
 
     def play_game(self):
-        self.num_of_players = input("How many players? ")
-        print(type(self.num_of_players))
+        pass
         
-
-        if self.num_of_players == "0":
-            self.player_1 = Human("Player1", 0)
-            self.AI_1 = Ai("Ai", 0)
-            print(self.player_1.wins)
-            print(self.AI_1.wins)
-        elif self.num_of_players == "1":
-            self.player_1 = Human("Player1", 0)
-            self.player_1 = Human("Player2",0)
-            print(self.player_1.wins)
-
-
-
- 
-
+            
+        
     def start_game(self):
         self.display_welcome()
         self.display_rules()
-        #self.choose_players()
+        self.choose_players()
         self.play_game()
